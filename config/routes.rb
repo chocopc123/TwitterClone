224d8219998b05517751333ee4bc864f3ac70199
choco <0215ryuuku@gmail.com>
2020-03-04 17:31:04 +0900

    投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
+  get "posts/:id" => "posts#show"
+  get "/" => "home#top"
   get "/about" => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
