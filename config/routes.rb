1332b4806b2f50de30d4fe64496cc0d8224ad224
choco <0215ryuuku@gmail.com>
2020-03-06 19:09:17 +0900

    いいね!一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "users/index" => "users#index"
   post "users/create" => "users#create"
   get "users/:id" => "users#show"
+  get "users/:id/likes" => "users#likes"
   get "users/:id/edit" => "users#edit"
   post "users/:id/update" => "users#update"
   get "posts/index" => "posts#index"
